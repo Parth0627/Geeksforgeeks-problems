26c141196bb7e7b84ee4684f24bd1c8cdc4f47a4
Parth0627 <pandyaparth271105@gmail.com>
2024-11-27 20:04:04 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%lu is the size of an integer in your system\n",sizeof(int));
+    
+    printf("%lu is the size of an float in your system\n",sizeof(float));
+    
+    printf("%lu is the size of an char in your system\n",sizeof(char));
+    
+    printf("%lu is the size of an double in your system\n",sizeof(double));
+    
+    return 0;
+}
