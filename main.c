d22ecdc8ee97b7872b03d7c2c0c7fbea1576930f
Parth0627 <pandyaparth271105@gmail.com>
2024-11-27 19:45:11 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1,num2;
+    
+    printf("Enter your first number\n");
+    scanf("%f",&num1);
+    
+     printf("Enter your first number\n");
+    scanf("%f",&num2);
+    
+    printf("%f * %f = %f",num1,num2,num1*num2);
+    
+    return 0;
+}
