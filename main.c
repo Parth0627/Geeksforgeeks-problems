84ff3e1ed878be33f51061c74765d251568f89ae
Parth0627 <pandyaparth271105@gmail.com>
2024-11-28 13:05:16 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("The alphabets from a to z:\n");  
+  for (int i = 97;i<=122;i++)
+  {
+       printf("%c ",(char)i);
+  }
+   printf("\n");
+  
+    return 0;
+}
