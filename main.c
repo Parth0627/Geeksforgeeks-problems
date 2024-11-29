9d15e25c3fbc79129999755b53bf53cb543eaf0d
Parth0627 <pandyaparth271105@gmail.com>
2024-11-29 16:56:59 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter your number\n");
+    scanf("%d",&num);
+    
+    for (int i = 1;i<=num;i++)
+    {
+        for (int j = 0;j<i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
